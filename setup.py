3f9049b4f352e6c49f0b7438cb19fffb3b2cd58f
Chet Luther <chet.luther@gmail.com>
2014-10-23 12:57:44 -0500

    Post Release: Version 2.1.0 -> 2.1.1dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = 'ZenPacks.zenoss.BrocadeMonitor'
-VERSION = '2.1.0'
+VERSION = '2.1.1dev'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
